57b65d20a62e66e6e8bd86d3cdeb95b1e5585504
Stefan Knaak <stefan@knaak.org>
2021-11-13 17:43:05 +0100

    03 c# kill process

---
+++ 03 c# kill process/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+
+
+namespace _03_c__kill_process
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            foreach(System.Diagnostics.Process myProc in System.Diagnostics.Process.GetProcesses())
+            {
+                if (myProc.ProcessName == "nsfminer")
+                {
+                    myProc.Kill();
+                    Console.WriteLine("nsfminer killed");
+                }
+            }
+        }
+    }
+}
